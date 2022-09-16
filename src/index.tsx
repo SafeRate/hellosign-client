5e230d0b1b16108147c9f2bb89a08dd77382cb96
Nathan Hall <hannahtall@yahoo.com>
2022-09-16 11:25:44 -0500

    First commit

---
+++ src/index.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import { createRoot } from "react-dom/client";
+
+const rootElement = document.getElementById("root");
+
+if (rootElement) {
+  const root = createRoot(rootElement);
+
+  root.render(
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>
+  );
+}
